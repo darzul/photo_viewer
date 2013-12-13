7555426c70a65350bbadfbd2f6766f05e1e4196f
Darzul <guillaumebourderye@gmail.com>
2013-12-13 22:06:03 +0100

    You can create an Album Some bugs with album display corrected

--- PhotoViewer/PhotoViewer/MainForm.cs
+++ PhotoViewer/PhotoViewer/MainForm.cs
@@ -12,75 +12,30 @@ namespace PhotoViewer
 {
     public partial class MainForm : Form
     {
-        AlbumUC AlbumUC2;
-        AlbumUC AlbumUC3;
-        List<AlbumUC> albums;
-        AlbumUC currentAlbumPrinted = null;
+        public static List<AlbumUC> albums = new List<AlbumUC> ();
 
         public MainForm()
         {
             InitializeComponent();
 
-            this.AlbumUC2 = new AlbumUC("C:\\Users\\Guillaume\\Pictures");
-            albumsFlowLayoutPanel.Controls.Add(AlbumUC2);
+            // Set the minimal size for the detailLayout
+            this.secondarySplitContainer.SplitterDistance = this.Height;
 
-            this.AlbumUC3 = new AlbumUC("C:\\Users\\Guillaume\\Pictures");
-            this.AlbumUC3.Click += displayPictures;
-            albumsFlowLayoutPanel.Controls.Add(this.AlbumUC3);
+            PictureUC.setDetailLayout(this.detailFlowLayoutPanel);
+
+            AlbumUC.setAlbumLayout(picturesFlowLayoutPanel);
         }
 
-        private void displayPictures(object sender, EventArgs e)
+        private void createAlbumToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (sender.ToString().Contains("AlbumUC"))
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
             {
-                if (sender.Equals(this.AlbumUC2))
-                {
-                    this.AlbumUC2.displayPictures(picturesFlowLayoutPanel);
-                    currentAlbumPrinted = AlbumUC2;
-                }
-                else if (sender.Equals(this.AlbumUC3))
-                {
-                    this.AlbumUC3.displayPictures(picturesFlowLayoutPanel);
-                    currentAlbumPrinted = AlbumUC3;
-                }
-
-                
+                var album = new AlbumUC(dialog.SelectedPath.ToString());
+                albums.Add(album);
+                albumsFlowLayoutPanel.Controls.Add(album);
             }
-        }
-
-        private bool needDisplayvScrollBar ()
-        {
-            //picturesFlowLayoutPanel.get
-            return true;
-        }
-
-        private void button1_Paint(object sender, PaintEventArgs e)
-        {
-            System.Drawing.Drawing2D.GraphicsPath buttonPath = new System.Drawing.Drawing2D.GraphicsPath();
-
-            //Set a new rectangle to the same size as the button's
-            //ClientRectangle property.
-            System.Drawing.Rectangle newRectangle = button1.ClientRectangle;
-
-            //Decrease the size of the rectangle
-            newRectangle.Inflate(-1, -1);
-
-            //Draw the button's border
-            e.Graphics.DrawEllipse(System.Drawing.Pens.CadetBlue, newRectangle);
-
-            //Increase the size of the rectangle to include the border
-            newRectangle.Inflate(1, 1);
-
-            //Create a circle within the new rectangle
-            buttonPath.AddEllipse(newRectangle);
-
-            //Set the button's region property to the newly created circle region
-            button1.Region = new System.Drawing.Region(buttonPath);
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            // Lancer le diapo
         }
     }
 }
--- PhotoViewer/PhotoViewer/PictureUC.cs
+++ PhotoViewer/PhotoViewer/PictureUC.cs
@@ -14,11 +14,17 @@ namespace PhotoViewer
     {
         private string title;
         private string path;
+        public static int widthPictureUC = 150;
+        public static int heightPictureUC = 180;
+        public AlbumUC album;
 
-        public PictureUC(string path)
+        private static FlowLayoutPanel detailLayout = null;
+        
+        public PictureUC(string path, AlbumUC album)
         {
             InitializeComponent();
 
+            this.album = album;
             this.path = path;
 
             this.pictureBox.Image = AlbumUC.ScaleImage(Image.FromFile(path), 100, 100);
@@ -29,6 +35,18 @@ namespace PhotoViewer
 
         public string getPath () {
             return this.path;
+        }
+
+        public static void setDetailLayout(FlowLayoutPanel layout)
+        {
+            PictureUC.detailLayout = layout;
+        }
+
+        private void PictureUC_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.AliceBlue;
+
+            
         }
     }
 }
