3cb797eb2d01fc63dc98568db7e09beec0f8c4ce
Tarek <smirani@et.esiea.fr>
2013-12-27 00:07:30 +0100

    merge

--- PhotoViewer/PhotoViewer/MainForm.cs
+++ PhotoViewer/PhotoViewer/MainForm.cs
@@ -29,7 +29,7 @@ namespace PhotoViewer
             xmlAlbums.Add(AlbumUC2);
 
             this.AlbumUC3 = new AlbumUC("C:\\Users\\Dev\\Pictures");
-            this.AlbumUC3.Click += displayPictures;
+            //this.AlbumUC3.Click += displayPictures;
             albumsFlowLayoutPanel.Controls.Add(this.AlbumUC3);
             xmlAlbums.Add(AlbumUC3);
 
@@ -61,29 +61,6 @@ namespace PhotoViewer
             return true;
         }
 
-        private void button1_Paint(object sender, PaintEventArgs e)
-        {
-            System.Drawing.Drawing2D.GraphicsPath buttonPath = new System.Drawing.Drawing2D.GraphicsPath();
-
-            //Set a new rectangle to the same size as the button's
-            //ClientRectangle property.
-            System.Drawing.Rectangle newRectangle = button1.ClientRectangle;
-
-            //Decrease the size of the rectangle
-            newRectangle.Inflate(-1, -1);
-
-            //Draw the button's border
-            e.Graphics.DrawEllipse(System.Drawing.Pens.CadetBlue, newRectangle);
-
-            //Increase the size of the rectangle to include the border
-            newRectangle.Inflate(1, 1);
-
-            //Create a circle within the new rectangle
-            buttonPath.AddEllipse(newRectangle);
-
-            //Set the button's region property to the newly created circle region
-            button1.Region = new System.Drawing.Region(buttonPath);
-        }
 
         private void button1_Click(object sender, EventArgs e)
         {
